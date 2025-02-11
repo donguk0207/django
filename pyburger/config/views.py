70568deec600ee2528458770da42bbbddcfeebfe
donguk0207 <donguk0207@gmail.com>
2025-02-11 17:48:23 +0900

    Django_test#2

--- pyburger/config/views.py
+++ pyburger/config/views.py
@@ -3,14 +3,21 @@ from django.shortcuts import render
 from burgers.models import Burger
 
 def index(request):
-    print("index")
     return render(request, template_name="main.html")
 
 def burger_list(request):
     burgers = Burger.objects.all()
     print("전체 햄버거 목록:", burgers)
-    data = {
-        "burgers": burgers,
-    }
 
-    return render(request, template_name="burger_list.html", context=data)
+    return render(request, "burger_list.html", {"burgers": burgers})
+
+def burger_search(request):
+    #keyword = request.GET["keyword"]
+    keyword = request.GET.get("keyword", None)
+
+    if keyword is not None:
+        burgers = Burger.objects.filter(name__contains=keyword)
+    else:
+        burgers = Burger.objects.none()
+
+    return render(request, "burger_search.html", {"burgers": burgers})
